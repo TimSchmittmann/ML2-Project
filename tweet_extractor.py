d94e71af119d32d41555dd5cb2af55bfc51f1f86
Tim Schmittmann <tim.schmittmann@gmx.de>
2018-10-28 21:54:33 +0100

    continue setup

--- tweet_extractor.py
+++ tweet_extractor.py
@@ -8,10 +8,10 @@ import csv
 import config
 
 # Create an Api instance.
-api = twitter.Api(consumer_key=CONSUMER_KEY,
-                  consumer_secret=CONSUMER_SECRET,
-                  access_token_key=ACCESS_TOKEN,
-                  access_token_secret=ACCESS_TOKEN_SECRET)
+api = twitter.Api(consumer_key=config.CONSUMER_KEY,
+                  consumer_secret=config.CONSUMER_SECRET,
+                  access_token_key=config.ACCESS_TOKEN,
+                  access_token_secret=config.ACCESS_TOKEN_SECRET)
 
 maxId = 1056286279258816512
 maxLen = 0
